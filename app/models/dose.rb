56d242dd8d2e4b4acc726e25de4f5a218c2c8fbe
Roy van Arkel <royvanarkel@msn.com>
2016-03-03 12:09:25 +0100

    add validations

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,8 @@
 class Dose < ActiveRecord::Base
   belongs_to :ingredient
   belongs_to :cocktail
+  validates :description, presence: true
+  validates :ingredient_id, presence: true
+  validates :cocktail_id, presence: true
+  validates_uniqueness_of :ingredient_id, :scope => :cocktail_id
 end
